ef718111e4226b3c88951ac03d4b201e3d4601b3
Cristobal Fuentealba <114311709+Kitobal@users.noreply.github.com>
2023-06-10 18:04:08 -0400

    Added Day 10

---
+++ Day 10/main.py
@@ -0,0 +1,42 @@
+from art import logo
+def add(n1,n2):
+    return n1 + n2
+
+def subtract(n1,n2):
+    return n1 - n2
+
+def multiply(n1,n2):
+    return n1 * n2
+
+def divide(n1,n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for op in operations:
+        print(op)
+    shouldContinue = True
+
+    while shouldContinue:
+
+        symbol = input("Type an operation symbol: ")
+        num2 = float(input("What's the next number?: "))
+        function = operations[symbol]
+        result = function(num1,num2)
+        print(f"{num1} {symbol} {num2} = {result}")
+        yesOrNo = input(f"Type'y' to continue calculatin with {result} or 'n' to start a new calculation: ")
+        if yesOrNo == "y":
+            num1 = result
+        else:
+            shouldContinue = False
+            calculator()
+
+calculator()
