eef4b4bee3cc63168f4fb8f73ab02758b3821a5b
Cristobal Fuentealba <114311709+Kitobal@users.noreply.github.com>
2023-06-24 18:04:03 -0400

    Updated Day 21
    The game now saves your highest score in the data.txt file.

--- Day 21/scoreboard.py
+++ Day 21/scoreboard.py
@@ -5,6 +5,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -20,6 +22,10 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def game_over(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
         self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("Arial", 24, "normal"))
+        self.write(f"GAME OVER \n Highscore: {self.highscore}", align="center", font=("Arial", 24, "normal"))
 
