3b4282d0562c2a6b1d36c270f7ec6a6457fe722d
Cristobal Fuentealba <114311709+Kitobal@users.noreply.github.com>
2023-06-27 13:35:09 -0400

    Update main.py

--- Day 25/main.py
+++ Day 25/main.py
@@ -14,10 +14,7 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f" {len(guessed_states)}/50 States", prompt="What's another state name?").title()
     if answer_state == "Exit":
-        not_guessed = []
-        for state in all_states:
-            if state not in guessed_states:
-                not_guessed.append(state)
+        not_guessed = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(not_guessed)
         new_data.to_csv("states_to_learn.csv")
         break
