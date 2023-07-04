25541ac0aaa071781ae1ecd95ab480bee1b1664e
Cristobal Fuentealba <114311709+Kitobal@users.noreply.github.com>
2023-07-04 16:41:06 -0400

    Update main.py

--- Day 26/main.py
+++ Day 26/main.py
@@ -7,6 +7,16 @@ df = pandas.DataFrame(pandas.read_csv("nato_phonetic_alphabet.csv"))
 nato_alphabet = {row.letter: row.code for (index, row) in df.iterrows()}
 # print(nato_alphabet)
 
-input_word = input("Enter a word: ")
-output_list = [nato_alphabet[letter] for letter in input_word.upper() if letter != " "]
-print(output_list)
+
+def generate_phonetic():
+    input_word = input("Enter a word: ")
+    try:
+        output_list = [nato_alphabet[letter] for letter in input_word.upper() if letter != " "]
+    except KeyError:
+        print("Only letters please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
